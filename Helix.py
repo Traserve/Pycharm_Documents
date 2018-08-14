59f312103151c8f2577a0420fcf72ccc349c92fa
Martin Don <1304873168@qq.com>
2018-08-14 19:48:32 +0800

    Helix Commit

---
+++ Helix.py
@@ -0,0 +1,12 @@
+import turtle
+import time
+turtle.pensize(2)
+turtle.bgcolor("black")
+colors = ["red", "yellow",'purple','blue']
+turtle.tracer(False)
+for i in range(400):
+    turtle.forward(2*i)
+    turtle.color(colors[i%4])
+    turtle.left(91)
+turtle.tracer(True)
+turtle.done()
