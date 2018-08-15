c50c0976efb59bee10ed95306e30f8ffae1fb69f
Martin <hitwhmartin@163.com>
2018-08-15 11:17:08 +0800

    Commit by Martin

--- ReadCsvTest.py
+++ ReadCsvTest.py
@@ -1,4 +1,16 @@
 import pandas as pd
 
 df = pd.read_csv('D:\\PycharmProjects\\tips.csv')
-print(df)
+# print(df.head()) # 打印数据前五行
+# print(df.tail()) # 打印数据后5行
+print(df.columns)  # 打印列名
+# print(df.columns[2])
+# print(df.index) # 打印列信息
+# print(df.ix[10:20, 0:3]) # 打印10~20行前三列数据
+# print(df.iloc[[1,3,5], [2,4]]) # 提取不连续行和列的数据，这个例子提取的是第1,3,5行，第2,4列的数据
+# print(df.iat[3, 2]) # 专门提取某一个数据，这个例子提取的是第三行，第二列数据（默认从0开始算哈）
+# print(df.drop([df.columns[0],df.columns[1]], axis = 1)) # 舍弃两列数据
+# print(df.drop(['total_bill', 'tip'], axis = 1)) # 舍弃两列数据
+# print(df.drop(columns=['total_bill', 'tip'], axis = 1)) # 舍弃两列数据
+# print(df.drop([0, 1], axis = 0)) # 舍弃两行数据
+# print(df.drop([0, 1])) # 舍弃两行数据
