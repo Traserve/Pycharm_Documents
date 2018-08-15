75774c18690fcf8d6da35d399c58962f171b181c
Martin <hitwhmartin@163.com>
2018-08-15 10:42:24 +0800

    Commit by Martin

--- pyMysqlTest.py
+++ pyMysqlTest.py
@@ -6,16 +6,22 @@ import pymysql
 # print(df)
 # mysql_cn.close()
 
+# 打开数据库连接
 db = pymysql.connect(host='localhost', port=3306, user='wht_dev', passwd='wht_dev', db='wht_dev')
 # df = pd.read_csv('D:\\PycharmProjects\\tips.csv')
+# 使用cursor()方法获取操作游标
 cur = db.cursor()
 sql = 'select * from hap_demo_b'
 df = pd.read_sql(sql, db)
-# index=False表示导出时去掉行名称，如果数据中含有中文，一般encoding指定为‘utf-8’
-df.to_csv('D:\\PycharmProjects\\hapDemoB.csv', encoding='utf-8', index=False)
+# index=False表示导出时去掉行名称，如果数据中含有中文，一般encoding指定为'gb18030','utf-8'测试无效，因该是Excel只识别gb编码
+# 日期数据导出后在Excel表格中显示为####是因为列宽太窄，增加列宽之后正常显示
+df.to_csv('D:\\PycharmProjects\\hapDemoB.csv', encoding='gb18030', index=False)
 try:
+    # 执行sql语句
     cur.execute(sql)
+    # 获取查询的所有记录
     results = cur.fetchall()
+    # 遍历结果
     for row in results:
         # print(row[0],' ',row[1],' ',row[2],' ',row[3],' ',row[4])
         print(row)
